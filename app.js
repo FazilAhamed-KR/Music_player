e5ab7e214008c30fa90f6a32e36c2fc14c85c24a
FazilAhamed-KR <110653442+FazilAhamed-KR@users.noreply.github.com>
2023-11-03 16:08:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -98,11 +98,10 @@ const UIController = (function () {
     divSonglist: ".song-list",
   };
 
-
-  var songUrls = [];
-  var songImageUrls = [];
-  var songTitle = [];
-  var songartists = [];
+var   songUrls = [];
+var   songImageUrls = [];
+var   songTitle = [];
+var   songartists = [];
 
   //public methods
   return {
@@ -135,18 +134,22 @@ const UIController = (function () {
 
    
   
-    // need method to create a track list group item
+    // Creating a Track Item in the Playlist
     createTrack(id, name,previewUrl,img,artists) {
-      //console.log(previewUrl);
-      // console.log(artists);
+
+
+      // Storing Song-Related Data
       songartists.push(artists)
       songTitle.push(name)
       songImageUrls.push(img)
       songUrls.push(previewUrl);
       
-  
-      
+
+      // Creating HTML Element for the Track
       const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`;
+
+      
+      // Displaying the Track in the User Interface
       document
         .querySelector(DOMElements.divSonglist)
         .insertAdjacentHTML("beforeend", html);
@@ -154,60 +157,51 @@ const UIController = (function () {
 
     // need method to create the song detail
     createTrackDetail(img, title, artist, songUrl) {
-      // console.log(artist);
-      //console.log(songUrls,'fayas222');
       const detailDiv = document.querySelector(DOMElements.divSongDetail);
 
       // any time user clicks a new song, we need to clear out the song detail div
       detailDiv.innerHTML = "";
 
       const html = `
-            <div class="container"> 
-            <div class="row justify-content-center">
-            <div class="music-player">
-            <div class="album-art text-center">
-            <div class="row col-sm-12 px-0">
-                <img src="${img}" alt="" id ="image">        
-            </div>
-            <div class="song-info text-center">
-                        <h5 id="title" >${title}</h5>
-                        <p id="artist">${artist}</p>
-                    </div>   
-            <div class="iconsContainer">
+            <div class="card" style="width: 18rem;"> 
+                <img src="${img}" class="card-img-top" alt="" id ="image">        
+                <div class="card-body">
+                        <h5 class="card-title" id="title" >${title}</h5>
+                        <p class="card-title" id="artist">${artist}</p>
+                        </div>
             <div class="icons">
-            <button id="prevButton" class="icon"><i class="bi bi-skip-backward-fill"></i></button>
+            <button id="prevButton" class="btn btn-primary"><i class="bi bi-skip-backward-fill"></i></button>
             <audio id="myAudio">
             <source src="${songUrl}" type="audio/mpeg">
              </audio>
-             <button id="playButton" class="icon" onclick="playAudio()"><i class="bi bi-play"></i></button>
-             <button id="nextButton" class="icon"><i class="bi bi-skip-forward-fill"></i></button>
+             <button id="playButton" class="btn btn-primary" onclick="playAudio()"><i class="bi bi-play"></i></button>
+             <button id="nextButton" class="btn btn-primary"><i class="bi bi-skip-forward-fill"></i></button>
         </div>
-        </div>
-        </div>
-         </div>
-         </div> 
-            </div>
-            </div>`;
+        </div>`;
 
       detailDiv.insertAdjacentHTML("beforeend", html);
 
+      // Adding Event Listeners for Next and Previous Buttons
       const nextButton = document.getElementById("nextButton");
       nextButton.addEventListener("click", playNextSong,false);
+      const prevButton = document.getElementById("prevButton");
+      prevButton.addEventListener("click", playPreviousSong,false);
+
+      // Passing Song-Related Data to Event Listeners
       nextButton.songUrls = songUrls;
       nextButton.songImageUrls = songImageUrls;
       nextButton.songTitle = songTitle;
       nextButton.songartists = songartists;
-      
-      const prevButton = document.getElementById("prevButton");
-      prevButton.addEventListener("click", playPreviousSong,false);
+
       prevButton.songUrls = songUrls;
       prevButton.songImageUrls = songImageUrls;
       prevButton.songTitle = songTitle;
       prevButton.songartists = songartists;
-    
+      
+      // Listening for the End of Audio Playback
       const audio = document.getElementById("myAudio");
       audio.addEventListener("ended", playNextSong, false);
-      // audio = songUrls
+      
     },
 
     resetTrackDetail() {
@@ -239,10 +233,10 @@ const UIController = (function () {
 
 
 // Define the song URLs
-
+// Functions for Playing Previous and Next Songs
 let currentSongIndex = 0;
-
 function playPreviousSong(evt) {
+   // Retrieve song-related data from the event object
   let songUrls = evt.currentTarget.songUrls;
   let songImageUrls = evt.currentTarget.songImageUrls;
   let songTitle = evt.currentTarget.songTitle;
@@ -252,28 +246,42 @@ function playPreviousSong(evt) {
     } else {
         currentSongIndex = songUrls.length - 1; // Wrap around to the last song if at the beginning of the list
     }
-    changeitem(currentSongIndex,songImageUrls,songartists);
-    changeTitle(currentSongIndex,songTitle);
+    // Update the song details (image, title, artist) for the previous song
+    changeitem(currentSongIndex,songImageUrls,songTitle,songartists);
     const audio = document.getElementById("myAudio");
     audio.src = songUrls[currentSongIndex];
     audio.play(); // Auto-play the previous song
 }
 
-function changeitem(currentSongIndex,songImageUrls,songartists) {
+function playNextSong(evt) {
+   // Retrieve song-related data from the event object
+  let songUrls = evt.currentTarget.songUrls;
+  let songImageUrls = evt.currentTarget.songImageUrls;
+  let songTitle = evt.currentTarget.songTitle;
+  let songartists = evt.currentTarget.songartists;
+    if (currentSongIndex < songUrls.length - 1) {
+        currentSongIndex++;
+    } else {
+        currentSongIndex = 0; // Wrap around to the first song if at the end of the list.
+    }
+    // Update the song details (image, title, artist) for the previous song.
+    changeitem(currentSongIndex,songImageUrls,songTitle,songartists);
+    const audio = document.getElementById("myAudio");
+    audio.src = songUrls[currentSongIndex];
+    audio.play(); // Auto-play the next song
+};  
+//This is the function for changing the songimg artists title.  
+function changeitem(currentSongIndex,songImageUrls,songTitle,songartists) {
   const image = document.getElementById("image")
   image.src = songImageUrls[currentSongIndex];
   const art = document.getElementById("artist")
   art.textContent = songartists[currentSongIndex];
-}
-
-function changeTitle(currentSongIndex,songTitle) {
   const Title = document.getElementById("title")
   Title.textContent = songTitle[currentSongIndex];
 }
 
 // Add event listeners to the next and previous buttons
-
-
+// Function to Play/Pause the Audio
 function playAudio() {
     const audio = document.getElementById("myAudio"); 
     const playButton = document.getElementById("playButton"); 
@@ -287,40 +295,9 @@ function playAudio() {
     }
   }
   
-  function playNextSong(evt) {
-
-    let songUrls = evt.currentTarget.songUrls;
-    let songImageUrls = evt.currentTarget.songImageUrls;
-    let songTitle = evt.currentTarget.songTitle;
-    let songartists = evt.currentTarget.songartists;
-    // console.log(evt.currentTarget.songUrls,'fayas111');
-      if (currentSongIndex < songUrls.length - 1) {
-          currentSongIndex++;
-      } else {
-          currentSongIndex = 0; // Wrap around to the first song if at the end of the list
-      }
-      changeitem(currentSongIndex,songImageUrls,songartists);
-      changeTitle(currentSongIndex,songTitle);
-      const audio = document.getElementById("myAudio");
-      audio.src = songUrls[currentSongIndex];
-      audio.play(); // Auto-play the next song
-  };  
-  // function changeImage(currentSongIndex,songImageUrls) {
-  //   const image = document.getElementById("image")
-  //   image.src = songImageUrls[currentSongIndex];
-  // }
-
-  // function changeTitle(currentSongIndex,songTitle) {
-  //   const Title = document.getElementById("title")
-  //   Title.textContent = songTitle[currentSongIndex];
-  // }
-
-
-
 const APPController = (function (UICtrl, APICtrl) {
   // get input field object ref
   const DOMInputs = UICtrl.inputField();
-
   // get genres on page load
   const loadGenres = async () => {
     //get the token
@@ -365,7 +342,6 @@ const APPController = (function (UICtrl, APICtrl) {
     // get the list of tracks
     const tracks = await APICtrl.getTracks(token, tracksEndPoint);
     // create a track list item
-    console.log(tracks,'tracks11')
     tracks.forEach((el) => UICtrl.createTrack(el.track.href, el.track.name,el.track.preview_url,el.track.album.images[1].url,el.track.artists[0].name));
   });
 
@@ -392,10 +368,6 @@ const APPController = (function (UICtrl, APICtrl) {
       track.preview_url
     );
   });
-
-
-
-
   return {
     init() {
       console.log("App is starting");
